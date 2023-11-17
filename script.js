73411ea98f3047e6413d485aa6e5aeabff4e6f3f
emy24107 <110105544+emy24107@users.noreply.github.com>
2023-11-17 08:11:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -48,6 +48,37 @@ function calcula(){
   }
 }
 
-
-
+let a = 0;
+let b = 0;
+let c = 0;
+function calcula_eq(){
+  if(a != 0){
+    let delta = (b*b) - (4*a*c);
+    document.getElementById("delta").innerHTML = delta;
+  }
+}
+function muda_sinal_a(){
+  a = (-1)*a;
+  calcula_eq();
+}
+function muda_sinal_b(){
+  b = (-1)*b;
+  calcula_eq();
+}
+function muda_sinal_c(){
+  c = (-1)*c;
+  calcula_eq();
+}
+function set_valor_a(){
+  a = document.getElementById("valor_a").value;
+  calcula_eq();
+}
+function set_valor_b(){
+  b = document.getElementById("valor_b").value;
+  calcula_eq();
+}
+function set_valor_c(){
+  c = document.getElementById("valor_c").value;
+  calcula_eq();
+}
 
